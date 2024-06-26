078274d5ece2dff36a758fefa71acfb436094f82
anthgg <cryptoanthdm@gmail.com>
2024-06-26 06:14:10 +0200

    changes

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,6 @@
 vim.opt.guicursor = "i:block"
 vim.opt.cursorline = false
+vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.list = false
--- lua/plugins/comp.lua
+++ lua/plugins/comp.lua
@@ -1,14 +1,24 @@
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      -- Merge the default options with your custom options
-      opts = vim.tbl_deep_extend("force", opts or {}, {
-        experimental = {
-          ghost_text = false,
-        },
-      })
-      return opts
-    end,
-  },
+  "hrsh7th/nvim-cmp",
+  config = function()
+    local cmp = require("cmp")
+    cmp.setup({
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+      }, {
+        { name = "buffer" },
+      }),
+    })
+  end,
 }
